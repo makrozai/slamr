db230ff3782a99b3e83fe8576e268391b367cc6d
Marco <marco.condoric@gmail.com>
2017-12-06 12:05:59 -0500

    creacion de envio de formulario con mailjs

--- js/bin/script.js
+++ js/bin/script.js
@@ -83,3 +83,35 @@ $(document).ready(function(){
     }
   });
 });
+
+/*mail send*/
+
+(function(){
+  emailjs.init("user_Zi381Px4VpQdKb3cOAPt2");
+})();
+
+
+var myform = $("form#myform");
+myform.submit(function(event){
+  event.preventDefault();
+
+  var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
+  console.log(params);
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+
+  var template_id = "makrozai";
+  myform.find("button").text("Sending...");
+  emailjs.send(service_id,template_id,params)
+    .then(function(){ 
+       alert("Sent!");
+       myform.find("button").text("Send");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
